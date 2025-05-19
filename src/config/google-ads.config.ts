b8397254097666e013a10c0242fb01a2f4af01e9
Andrew de Souza ribeiro <andrewsouza57@gmail.com>
2025-05-19 18:36:55 -0300

    feat: create new api google ads, integration

---
+++ src/config/google-ads.config.ts
@@ -0,0 +1,9 @@
+export default () => ({
+  googleAds: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    redirectUri: process.env.GOOGLE_REDIRECT_URI,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    developerToken: process.env.GOOGLE_DEVELOPER_TOKEN,
+  },
+});
