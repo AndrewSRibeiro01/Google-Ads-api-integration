e358ef429dd9df171b9c32d4f670d096ae7ee5cc
Andrew de Souza ribeiro <andrewsouza57@gmail.com>
2025-06-12 15:26:57 -0300

    fix: add new functions

--- src/auth/google-auth.controller.ts
+++ src/auth/google-auth.controller.ts
@@ -1,42 +1,39 @@
+import { TagManagerService } from './../tagmanager/tagmanager.service';
+import { GoogleAuthService } from './../auth/google-auth.service';
 import { Controller, Get, Query, Res } from '@nestjs/common';
-import { GoogleAuthService } from './google-auth.service';
-import { TagManagerService } from '../tagmanager/tagmanager.service';
 import { Response } from 'express';
 
 @Controller('auth')
 export class GoogleAuthController {
     constructor(
-        private readonly googleAuthService: GoogleAuthService,
-        private readonly tagManagerService: TagManagerService,
-    ) { }
+        private readonly GoogleAuthService: GoogleAuthService,
+        private readonly TagManagerService: TagManagerService,
+    ) {}
 
     @Get('login')
     async login(@Res() res: Response) {
-        const url = this.googleAuthService.generateAuthUrl();
+        const url = this.GoogleAuthService.generateAuthUrl();
         return res.redirect(url);
     }
 
     @Get('callback')
     async callback(@Query('code') code: string, @Res() res: Response) {
-        const { tokens } = await this.googleAuthService.getTokensFromCode(code);
+        const { tokens } = await this.GoogleAuthService.getTokensFromCode(code);
         console.log("Token de acesso:", tokens);
 
         if (!tokens.access_token) {
             return res.status(400).json({ message: 'Token de acesso ausente' });
         }
 
-        const accounts = await this.tagManagerService.listAccounts(tokens.access_token);
+        const accounts = await this.TagManagerService.listAccountsAndContainers(tokens);
 
         if (!accounts.length) {
             return res.status(404).json({ message: 'Nenhuma conta GTM encontrada para o usuário.' });
         }
 
-        const accountId = accounts[0].accountId;
-        const container = await this.tagManagerService.createContainer(tokens.access_token, accountId);
-
         return res.json({
-            message: 'Container criado com sucesso!',
-            container,
+            message: 'Contas e containers',
+            data: accounts,
         });
     }
 }
